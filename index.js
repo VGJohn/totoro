1fe27f169a8877bbdf2559a43fb11ce2851bc3aa
lexod <s34recovery@gmail.com>
2019-01-16 21:00:33 -0500

    Add middleware support

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-var express = require('express');
-var router = express.Router();
-var winston = require('winston');
-var consts = require('./consts.js');
-var Endpoint = require('./objects/Endpoint.js');
-var clone = require('clone');
-var bodyParser = require('body-parser')
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const router = require('express').Router();
+const winston = require('winston');
+const consts = require('./consts.js');
+const Endpoint = require('./objects/Endpoint.js');
 
 winston.level = consts.LOG_LEVEL;
 
-var rain = function(apiConfig, winstonLogger) {
-    enableLogging(winstonLogger);
+const defaultMiddleware = (res, req, next) => next();
 
-    var versions = {};
-    var previousApiVersion = null;
+const rain = (apiConfig) => {
 
-    for (var apiVersion in apiConfig) {
+    const versions = {};
+    let previousApiVersion = null;
+
+    for (let apiVersion in apiConfig) {
         if (apiConfig.hasOwnProperty(apiVersion)) {
-            var apiVersionConfig = apiConfig[apiVersion];
+            let apiVersionConfig = apiConfig[apiVersion];
 
-            var apiVersionActive = apiVersionConfig.active;
+            let apiVersionActive = apiVersionConfig.active;
             // use default value if not found
             if (apiVersionActive == null) apiVersionActive = true;
 
-            var apiVersionDeprecated = apiVersionConfig.deprecated;
+            let apiVersionDeprecated = apiVersionConfig.deprecated;
             // use default value if not found
             if (apiVersionDeprecated == null) apiVersionDeprecated = false;
 
@@ -37,19 +34,19 @@ var rain = function(apiConfig, winstonLogger) {
             // set previous api version number
             previousApiVersion = apiVersion;
 
-            for (var i = 0; i < apiVersionConfig.endpoints.length; i++) {
-                var endpointActive = apiVersionConfig.endpoints[i].active;
+            for (let i = 0; i < apiVersionConfig.endpoints.length; i++) {
+                let endpointActive = apiVersionConfig.endpoints[i].active;
                 // use default value if not found
                 if (endpointActive == null) endpointActive = true;
 
-                var endpointDeprecated = apiVersionConfig.endpoints[i].deprecated;
+                let endpointDeprecated = apiVersionConfig.endpoints[i].deprecated;
                 // use default value if not found
                 if (endpointDeprecated == null) endpointDeprecated = false;
 
                 apiVersionConfig.endpoints[i].active = endpointActive && apiVersionActive;
                 apiVersionConfig.endpoints[i].deprecated = endpointDeprecated || apiVersionDeprecated;
-                var endpoint = new Endpoint(apiVersion, apiVersionConfig.endpoints[i]);
-
+                let endpoint = new Endpoint(apiVersion, apiVersionConfig.endpoints[i]);
+                endpoint.config.middleware = [...[defaultMiddleware], ...(endpoint.config.middleware || [])];
                 // add new endpoint to the list or replace if it exists already
                 pushOrReplaceRoute(versions[apiVersion], endpoint);
             }
@@ -57,23 +54,16 @@ var rain = function(apiConfig, winstonLogger) {
     }
 
     return populateRouter(versions);
-}
-
-function enableLogging(winstonLogger) {
-    if (winstonLogger == null || winstonLogger == undefined) return;
-
-    winston = winstonLogger;
-}
-
+};
 
 function inheritEndpoints(versions, previousApiVersion, apiVersion) {
     if (previousApiVersion == null) {
         return;
     }
 
-    for (var i = 0; i < versions[previousApiVersion].length; i++) {
+    for (let i = 0; i < versions[previousApiVersion].length; i++) {
         if (!versions[previousApiVersion][i].config.deprecated) {
-            var endpointCopy = clone(versions[previousApiVersion][i]);
+            let endpointCopy = { ...versions[previousApiVersion][i] };
             endpointCopy.apiVersion = apiVersion;
             endpointCopy.config.active = true;
             versions[apiVersion].push(endpointCopy);
@@ -82,8 +72,8 @@ function inheritEndpoints(versions, previousApiVersion, apiVersion) {
 }
 
 function pushOrReplaceRoute(endpoints, endpoint) {
-    var replaced = false;
-    for (var i = 0; i < endpoints.length; i++) {
+    let replaced = false;
+    for (let i = 0; i < endpoints.length; i++) {
         if (endpoints[i].config.route == endpoint.config.route
             && endpoints[i].config.method == endpoint.config.method) {
             endpoints[i] = endpoint;
@@ -97,15 +87,15 @@ function pushOrReplaceRoute(endpoints, endpoint) {
 }
 
 function populateRouter(versions) {
-    for (var apiVersion in versions) {
+    for (let apiVersion in versions) {
         if (versions.hasOwnProperty(apiVersion)) {
-            winston.debug('Adding routes for API version', apiVersion);
-            for (var i = 0; i < versions[apiVersion].length; i++) {
+            winston.debug(`Start of API version`, apiVersion);
+            for (let i = 0; i < versions[apiVersion].length; i++) {
                 if (versions[apiVersion][i].config.active) {
                     constructRoute(versions[apiVersion][i]);
                 }
             }
-            winston.debug('');
+            winston.debug(`End of API version`, apiVersion, "\n");
         }
     }
 
@@ -113,34 +103,18 @@ function populateRouter(versions) {
 }
 
 function constructRoute(endpoint) {
-    switch (endpoint.config.method) {
-        case consts.HTTP_GET:
-            winston.debug('Adding route \'' + endpoint.config.method, ' /' + endpoint.apiVersion + endpoint.config.route + '\'');
-            router.get('/' + endpoint.apiVersion + endpoint.config.route, function(req, res, next) {
-                endpoint.config.implementation(endpoint.apiVersion, req, res, next);
-            });
-            break;
-        case consts.HTTP_POST:
-            winston.debug('Adding route \'' + endpoint.config.method, ' /' + endpoint.apiVersion + endpoint.config.route + '\'');
-            router.post('/' + endpoint.apiVersion + endpoint.config.route, urlencodedParser, function(req, res, next) {
-                endpoint.config.implementation(endpoint.apiVersion, req, res, next);
-            });
-            break;
-        case consts.HTTP_DELETE:
-            winston.debug('Adding route \'' + endpoint.config.method, ' /' + endpoint.apiVersion + endpoint.config.route + '\'');
-            router.delete('/' + endpoint.apiVersion + endpoint.config.route, function(req, res, next) {
-                endpoint.config.implementation(endpoint.apiVersion, req, res, next);
-            });
-            break;
-        case consts.HTTP_PUT:
-            winston.debug('Adding route \'' + endpoint.config.method, ' /' + endpoint.apiVersion + endpoint.config.route + '\'');
-            router.put('/' + endpoint.apiVersion + endpoint.config.route, function(req, res, next) {
-                endpoint.config.implementation(endpoint.apiVersion, req, res, next);
-            });
-            break;
-        default:
-            winston.debug('HTTP Method not recognised! Not adding endpoint \'' + endpoint.config.method, ' /' + endpoint.apiVersion + endpoint.config.route + '\'');
+    const RouteFunction = (req, res, next) => {
+            req.apiVersion = endpoint.apiVersion;
+            return endpoint.config.implementation(req, res, next);
+        },
+        endpointURL = `/${endpoint.apiVersion}${endpoint.config.route}`;
+
+    if(!consts.HTTP_METHODS.includes(endpoint.config.method)){
+        winston.error(`HTTP Method not recognised! '${endpoint.config.method} ${endpointURL}'`);
+        return;
     }
+    winston.debug(`Adding route '${endpoint.config.method} ${endpointURL}'`);
+    router[endpoint.config.method.toLowerCase()](endpointURL, endpoint.config.middleware, RouteFunction);
 }
 
 module.exports = {
