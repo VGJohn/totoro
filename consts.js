6036d2bf780e49d86cb85cebee7ad5e4c67dea11
tusharpandey13 <tusharpandey13@gmail.com>
2020-06-30 11:00:05 +0530

    11:00:05

--- consts.js
+++ consts.js
@@ -1,22 +1,4 @@
-const HTTP_GET      = "GET";
-const HTTP_POST     = "POST";
-const HTTP_DELETE   = "DELETE";
-const HTTP_PUT      = "PUT";
-const HTTP_PATCH    = "PATCH";
-const LOG_LEVEL     = "debug";
-
 module.exports = {
-    HTTP_METHODS: [
-        HTTP_GET,
-        HTTP_POST,
-        HTTP_DELETE,
-        HTTP_PUT,
-        HTTP_PATCH,
-    ],
-    HTTP_GET: HTTP_GET,
-    HTTP_POST: HTTP_POST,
-    HTTP_DELETE: HTTP_DELETE,
-    HTTP_PUT: HTTP_PUT,
-    HTTP_PATCH: HTTP_PATCH,
-    LOG_LEVEL: LOG_LEVEL
+	HTTP_METHODS: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+	LOG_LEVEL: 'debug',
 };
